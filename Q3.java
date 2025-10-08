d0a5bfcb7d2a86653cd9553c088822feb45a52fa
Ted <123252087+tedht@users.noreply.github.com>
2025-10-08 14:04:11 +0000

    Update Q3.java

--- Q3.java
+++ Q3.java
@@ -1,1 +1,19 @@
+public class Q3
+{
+    public static void main(String[] args)
+    {
+        if(args.length != 1) return;
 
+		int N;
+        String suite;
+
+		N = Integer.parseInt(args[0]);
+		suite = "";
+
+		for(int i = 1;i <= N; i++)
+			suite += Integer.toString(i) + ' ';
+
+		System.out.println(suite);
+        
+    }
+}
